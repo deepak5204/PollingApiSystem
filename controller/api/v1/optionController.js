52aaa4b75cf06b82a22a3c9a875757dc47d492b9
Deepak <deepakkumar750206@gmail.com>
2023-01-31 14:58:32 +0530

    Successfully created option for given question's id

--- controller/api/v1/optionController.js
+++ controller/api/v1/optionController.js
@@ -4,8 +4,34 @@ const Option = require('../../../models/options');
 
 //create option for question using provided id
 module.exports.createOption = async function(req, res){
-    console.log("create option");
-    
+    // console.log("create option");
+    try{
+        let id = req.params.id;
+        let question = await Question.findById(id);
+        
+        if(question){
+           let option = await Option.create({
+            content: req.body.content,
+            votes: req.body.votes,
+            question: req.params.id,
+           });
+           option.link_vote = "http://localhost:8000/api/v1/options/" + option.id + "/add_vote";
+           option.save();
+           question.options.push(option);
+           question.save();
+
+           return res.json({
+            option,
+            data: {
+                message: "option created",
+            },
+           });
+        }
+        return res.json({question});
+    } catch(err){
+        console.log("Error : ",err);
+        return;
+    }    
 }
 
 
