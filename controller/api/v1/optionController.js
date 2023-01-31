bbc2cbca85ba772549dea39de313dcaa0f5bd413
Deepak <deepakkumar750206@gmail.com>
2023-01-31 23:42:33 +0530

    create delete route and add vote route

--- controller/api/v1/optionController.js
+++ controller/api/v1/optionController.js
@@ -32,19 +32,74 @@ module.exports.createOption = async function(req, res){
         console.log("Error : ",err);
         return;
     }    
-}
+};
 
 
 
 //delete an option on the basis of its id
-module.exports.optionDelete = async function(re, res){
-    console.log("delete options");
+module.exports.optionDelete = async function(req, res){
+    // console.log("delete options");
+    try{
+        let id = req.params.id;
 
+        //chek and find option exits or not
+        let option = await Option.findById(id);
+        console.log(option);
+        
+
+        // if option present then check for vote
+        // if option has vote then don't delete
+        if(option.votes > 0){
+            return res.status(404).json({
+                data: {
+                    message: "Can't delete! It has vote"
+                },
+            });
+        }
+
+        // delete option from Question's options array
+        await Question.findByIdAndUpdate(option.question, {
+            $pull: {options: id },
+        });
+
+        // delete option from option
+        await Option.findByIdAndDelete(id);
+        return res.status(200).json({
+            data: {
+                message: "Option deleted successfully"
+            },
+        });
+    }
+    catch (err) {
+        return res.status(500).json({
+            data: {
+                message: "Internal server error"
+            },
+        });
+    }
 };
 
 
 //adding vote to an option for particular question 
 module.exports.addVote = async function(req, res) {
     console.log("add vote");
+    try {
+        let id = req.params.id;
+
+        //find option if present then vote to it
+        await Option.findByIdAndUpdate(id, {$inc: { votes : 1}});
+
+        return res.status(200).json({
+            data: {
+                message: "Voted Successfully"
+            },
+        });
+    } catch (err){
+        return res.status(500).json({
+            data: {
+                message: "Internal server error"
+            },
+        });
+    } 
 };
 
