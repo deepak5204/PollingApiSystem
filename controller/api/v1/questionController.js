98875035be2a2e13f48a20855d9ac464e452c724
Deepak <deepakkumar750206@gmail.com>
2023-03-22 21:37:31 +0530

    Create a route for view all questions

--- controller/api/v1/questionController.js
+++ controller/api/v1/questionController.js
@@ -1,82 +1,103 @@
-const Question = require('../../../models/questions');
+const Question = require("../../../models/questions");
 const Option = require("../../../models/options");
 
 //create question
-module.exports.createQuestion = async function(req, res) {
-    console.log("create Question");
-    try {
-        let question = await Question.create(req.body);
-        
-        //if question created then return json response
-        if(question){
-            return res.json({
-                question,
-                data: { message: "question created successfully" },
-            });
-        } else {
-            // if no question created return error response
-            return res.status(500).json({
-                data: { message: "Internal server error"},
-            });
-        } 
-    } catch (err){
-        console.log("Error while creating question", err);
-        return;
+module.exports.createQuestion = async function (req, res) {
+  console.log("create Question");
+  try {
+    let question = await Question.create(req.body);
+
+    //if question created then return json response
+    if (question) {
+      return res.json({
+        question,
+        data: { message: "question created successfully" },
+      });
+    } else {
+      // if no question created return error response
+      return res.status(500).json({
+        data: { message: "Internal server error" },
+      });
     }
+  } catch (err) {
+    console.log("Error while creating question", err);
+    return;
+  }
 };
 
 //View question
-module.exports.viewQuestion = async function(req, res) {
-    // console.log("view Question");
-    try{
-        let question = await Question.findById(req.params.id).populate("options");
-        return res.json({ question });
-    } catch (err) {
-        return res.status(500).json({
-            data: {
-                message: "Internal server error for viewing question",
-            },
-        });
-    }
+module.exports.viewQuestion = async function (req, res) {
+  // console.log("view Question");
+  try {
+    let question = await Question.findById(req.params.id).populate("options");
+    res.status(200).json({
+      message: "Success",
+      data: {
+        question
+      }
+    });
+  } catch (err) {
+    res.status(500).json({
+        status: 'fail',
+      data: {
+        message: "Internal server error for viewing question",
+      },
+    });
+  }
 };
 
-
-// Delete question
-module.exports.deleteQuestion = async function(req, res) {
-    console.log("delete Question");
+// vieq all question
+module.exports.getAllQuestions = async (req, res) =>{
+    // console.log('View all question'); 
     try{
-        let id = req.params.id;
-        let question = await Question.findById(id).populate({
-            path: "options",
-            select: "votes",
-        });
-
-        if(question){
-            
-            let options = question.options;
-
-            for(let i = 0; i < options.length; i++){
-                if(options[i].votes > 0){
-                    return res.status(404).json({
-                        data: {
-                            message: "question option has some votes, Not Possible to delete",
-                        },
-                    });
-                }
+        const questions = await Question.find(); 
+        res.status(200).json({
+            message:'Success',
+            data: {
+                questions
             }
-            
-            await Option.deleteMany({question: id});
-            await Question.findByIdAndDelete(id);
-
-            return res.status(200).json({
-                message: "Question deleted successfully",
-            });
-        } else {
-            return res.status(404).json({message: "Question not found"});
-        }
-    } catch (err) {
-        return res.status(500).json({
-            message: "Internal server error, deleting question"
-        });
+        })
+    } catch (err){
+        console.log(err);
+        
     }
 }
+
+// Delete question
+module.exports.deleteQuestion = async function (req, res) {
+  console.log("delete Question");
+  try {
+    let id = req.params.id;
+    let question = await Question.findById(id).populate({
+      path: "options",
+      select: "votes",
+    });
+
+    if (question) {
+      let options = question.options;
+
+      for (let i = 0; i < options.length; i++) {
+        if (options[i].votes > 0) {
+          return res.status(404).json({
+            data: {
+              message: "question option has some votes, Not Possible to delete",
+            },
+          });
+        }
+      }
+
+      await Option.deleteMany({ question: id });
+      await Question.findByIdAndDelete(id);
+
+      return res.status(200).json({
+        message: "Question deleted successfully",
+      });
+    } else {
+      return res.status(404).json({ message: "Question not found" });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      message: "Internal server error, deleting question",
+    });
+  }
+};
--- routes/api/v1/question.js
+++ routes/api/v1/question.js
@@ -8,6 +8,8 @@ router.post('/create_question', questionController.createQuestion);
 
 router.get('/:id/view', questionController.viewQuestion);
 
+router.get('/getAll', questionController.getAllQuestions)
+
 router.delete('/:id/delete', questionController.deleteQuestion);
 
 
