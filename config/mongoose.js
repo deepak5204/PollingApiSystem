b5665ae23438f1449a45bda80141be3daf14e168
Deepak <deepakkumar750206@gmail.com>
2023-03-24 09:16:58 +0530

    Update code to remove deprecation warning

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.set('strictQuery', true);
+
 mongoose.connect('mongodb://127.0.0.1:27017/polling-api')
   .then(()=>{
     console.log('Connected!')
